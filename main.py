35399a8862decdabb0a9701bc5da62cdebbc9d45
disgoraj <disgoraj@gmail.com>
2025-04-06 08:33:54 +0000

    Added modified fiel

--- main.py
+++ main.py
@@ -330,5 +330,5 @@ async def get_advice(
 
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.getenv("PORT", 8080))
+    port = int(os.getenv("PORT", 8000))
     uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
